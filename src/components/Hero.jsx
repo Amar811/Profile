8bd2825cb590ce852ef6473ff54753afd1e77ec9
Amar Ram <ramamar151@gmail.com>
2025-07-11 18:48:32 +0530

    changes in hero files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
           <div className="flex items-center gap-3">
             <figure className="img-box w-9 h-9 rounded-lg">
               <img
-                src="public/images/amarimg.jpeg"
+                src="/images/amarimg.jpeg"
                 width={40}
                 height={40}
                 alt="Amar Ram"
